5e75ab058811963991997b2a3174b22e9162222b
Lucas Hendren <lhendre2@gmail.com>
2018-02-27 16:53:35 -0800

    added in readMe and cleaned code somewhat

--- PythonClientV1/ClientGateKeeper/gatekeeper/views.py
+++ PythonClientV1/ClientGateKeeper/gatekeeper/views.py
@@ -8,7 +8,6 @@ from web3 import Web3, HTTPProvider
 from ethereum.utils import ecrecover_to_pub, sha3
 from eth_utils.hexidecimal import encode_hex, decode_hex, add_0x_prefix
 import codecs
-# from .models import Episodes
 from gatekeeper.models import Details
 from django.forms.models import model_to_dict
 
@@ -22,7 +21,7 @@ def index(request):
     return HttpResponse("Hello, world. You're at the gatekeeper")
 
 def data(request, address_id, signature_id, message_hash, parameter, key):
-    keyHex=key#web3.fromAscii(key)
+    keyHex=key
     parameterHex=web3.fromAscii(parameter)
     keyHexData=keyHex[2:]
     parameterHexData=parameterHex[2:]
@@ -30,47 +29,28 @@ def data(request, address_id, signature_id, message_hash, parameter, key):
 
     key2=web3.toBytes(hexstr=keyHex)
     parameter2=web3.toBytes(hexstr=parameterHex)
-#isKeyOwner
 
     signer = address_id
     message_hash =message_hash
     signature = signature_id
-    # signer = "0x9283099a29556fcf8fff5b2cea2d4f67cb7a7a8b"
-    # message_hash = "0x6e099d83ea72d1ef62e39a501fe000c1458ba5a511510a0e9348b0dfeb298803"
-    # signature = "0x0cf7e2e1cbaf249175b8e004118a182eb378a0b78a7a741e72a0a34e970b59194aa4d9419352d181a4d1827abbad279ad4f5a7b60da5751b82fec4dde6f380a51b"
     r = int(signature[0:66], 16)
     s = int(add_0x_prefix(signature[66:130]), 16)
     v = int(add_0x_prefix(signature[130:132]), 16)
     if v not in (27,28):
         v += 27
     pubkey = ecrecover_to_pub(decode_hex(message_hash), v, r, s)
-    print(':                  pubkey              :')
-    print(pubkey)
-    print(signer)
-    print(encode_hex(sha3(pubkey)[-20:]))
-    print(':                  pubkey              :')
+
     accountID=fContract.call().getKeyData(key2,parameter2)
     owner=fContract.call().isKeyOwner(key2,address_id)
-    print('is owner: ',owner)
     hexId=web3.fromAscii(accountID)
-    print('ascii to hex',hexId)
     accountID=int(hexId.rstrip("0"), 16)
 
-    # episodes=Episodes.objects.filter(account_number=accountID)
-    print('return value:  ',accountID)
-    # signer = w3.eth.account.recover(message_hash, signature=signature_id)
-    episodes='test'
     result=Details.objects.filter(account_number=accountID)
-    # for item in result:
-    #     item['details'] = model_to_dict(item['details'])
-    # print(result)
+
     dataResult = serializers.serialize('json', result)
-    print(dataResult)
     if encode_hex(sha3(pubkey)[-20:]) == signer and owner:
         print(':                  success              :')
         return JsonResponse(dataResult, safe=False)
-
-        # return HttpResponse("You're looking at %s , %s , %s . Here is the data %s" % (address_id, signature_id, parameter,episodes))
     else:
         print(':                  fail              :')
         return JsonResponse({error:"Invalid User"})
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # GateKeeper-Python
+
+# Installation
+    Mac.  This installation assumes you already have psql installed.  Additionally, you can choose to skip step 1 if you already have python3 and the virtual environment installed or you have your own process
+      1.  Install Python3 and virtual Environment
+        a. brew search python
+        b. brew install python3
+        c. python3 --version
+        d. pip install virtualenv
+        e. pip install virtualenvwrapper
+        f. mkdir ~/.virtualenvs
+        g.  Open your .bashrc file and add:
+                export WORKON_HOME=~/.virtualenvs
+                source /usr/local/bin/virtualenvwrapper.sh
+        h. source .bashrc
+        i.  To create virtual environment
+        j.  mkvirtualenv --python=python3_path myenv
+    2. pip install Django
+    3. Git clone or download repo
+    4. Install Pythereum.  May need to use other commands based on system
+        a. sudo apt-get install libssl-dev build-essential automake pkg-config libtool libffi-dev libgmp-dev libyaml-cpp-dev
+        b. git clone https://github.com/ethereum/pyethereum/
+        c. cd pyethereum
+        d. python setup.py install
+    5.  Install web3
+        Mac:  pip install web3
+    6.  Create Database or determine parameters for the code to handle the database. The code is setup for PSQL with a database called drsdb and user drs with  password abcd12345.  Additionally we have it setup to handle data objects with the field as so account_number, name, details, days, pub_date.
+    7.  Database migration.  To either use our database or your own run python manage.py migrate.  This will load what ever tables you have into psql.  For other setups or downloading your DBs into django please review the Django manuals.
+
+# Running
+    1. python manage.py runserver
+
+
+# Summary
+  this code allows you to pass paramaters to the gatekeeper endpoint as so
+    gatekeeper/<str:address_id>/<str:signature_id>/<str:message_hash>/<str:parameter>/<str:key>
+  to access data.  This code is 0.1V it is usable but more testing and cleaning will need to be done.  Please notify us on github if any issues are found.
